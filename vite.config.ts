6541839400961852d45ee5bd3c0df4da1cead562
Vihaan Dharia <vihaandharia04@gmail.com>
2024-12-29 23:24:27 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'frontend/dist',  // Ensure the output directory is set to 'frontend/dist'
+  },
+})
